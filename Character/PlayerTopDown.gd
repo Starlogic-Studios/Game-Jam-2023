1f671fb4d9a7dc2290df3f5fa1df9d6fc30c25d6
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-07-07 16:46:01 -0500

    added friction and accel

--- Character/PlayerTopDown.gd
+++ Character/PlayerTopDown.gd
@@ -1,12 +1,30 @@
 extends CharacterBody2D
 
+const max_speed = 400
+const accel = 1500
+const friction = 600
 
-var speed = 400  # move speed in pixels/sec
-var rotation_speed = 1.5  # turning speed in radians/sec
+var input = Vector2.ZERO
 
 func _physics_process(delta):
-	var move_input = Input.get_axis("down", "up")
-	var rotation_direction = Input.get_axis("left", "right")
-	velocity = transform.x * move_input * speed
-	rotation += rotation_direction * rotation_speed * delta
+	player_movement(delta)
+
+func get_input():
+	input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	input.y= int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	return input.normalized()
+
+func player_movement(delta):
+	input = get_input()
+	# Adding friction
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
+	# Adding acceleration
+	else: 
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(max_speed)
+	
 	move_and_slide()
