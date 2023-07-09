5edb0b3dc144ab5977ccb2eb0338bc8f291665d1
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-07-09 02:02:59 -0500

    redone losses from merge

--- Character/Projectiles/Missile.gd
+++ Character/Projectiles/Missile.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var speed = 50
-@export var mass = 1
+@export var damage = 10
 
 var explosion_scene = preload("res://Character/Projectiles/explosion.tscn")
 var velocity = Vector2.ZERO
@@ -11,8 +11,11 @@ func _physics_process(delta):
 	position += velocity * delta
 
 func _on_Missile_body_entered(body):
-	if body.has_method("take_mass"):
-		body.take_damage(mass)
+	if body.get_parent().has_method("take_damage"):
+		body.get_parent().take_damage(damage)
+	var explosion = explosion_scene.instantiate()
+	explosion.global_position = global_position
+	get_parent().add_child(explosion)
 	queue_free()
 
 func _on_Lifetime_timeout():
