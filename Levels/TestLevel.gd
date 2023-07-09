b1f7880070f77b58d1f384e95a107a4219a1f58f
starlogic <christopher.simms38@gmail.com>
2023-07-08 21:45:32 -0500

    Working parallax and player

--- Levels/TestLevel.gd
+++ Levels/TestLevel.gd
@@ -5,13 +5,13 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	camera.position = Vector2.ZERO
-	pass # Replace with function body.
+# func _ready():
+# 	camera.position = Vector2.ZERO
+# 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	while camera.position.y > -28000:
-		await get_tree().create_timer(0.3).timeout  # Wait for 2 seconds
-		camera.position.y -= player.characterVelocity
+#func _process(_delta):
+#	while camera.position.y > -28000:
+#		await get_tree().create_timer(0.3).timeout  # Wait for 2 seconds
+#		camera.position.y -= player.characterVelocity
